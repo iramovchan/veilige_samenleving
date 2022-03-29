2a4fbef5afd5bc6a8f23b7ffa57a92c51bae4ce5
iramovchan <86199439+iramovchan@users.noreply.github.com>
2022-03-29 10:54:36 +0200

    some changes

--- game.js
+++ game.js
@@ -3,15 +3,26 @@ var yCnsSize = 500;
 
 var xRobot = xCnsSize/2;
 var yRobot = yCnsSize/2;
-//
-//var widthCan = 54;
-//var heightCan = 62;
+
+var robotWidth = 64;
+var robotHeight = 64;
+
+var blikjeWidth = 54;
+var blikjeHeight = 62;
+
+var xBlikje = 100;
+var yBlikje = 200;
 
 var move = 2;
+var blikjeMove = 2;
 
 var cns;
 
 var radius = 40;
+
+var colliding = false;
+
+var can_collected = false;
 
 function centerCanvas() {
     var xCns = (windowWidth - width) / 2;
@@ -20,9 +31,13 @@ function centerCanvas() {
 }
 
 let img;
+let robot_img;
+
 function preload() {
     img = loadImage('can.png');
+    robot_img = loadImage("robot.png");
 }
+
 
 function setup() {
     preload();
@@ -35,66 +50,163 @@ function windowResized() {
     centerCanvas();
 }
 
+//class Can() {
+//    constructor(img, xBlikje, yBlikje, blikjeWidth, blikjeHeight){
+//        this.img = img;
+//        this.xBlikje = xBlikje;
+//        this.yBlikje = yBlikje;
+//        this.blikjeWidth = blikjeWidth;
+//        this.blikjeHeight = blikjeHeight;
+//    }
+//}
+//
+//can_1 = new Can(img, xBlikje, yBlikje, blikjeWidth, blikjeHeight);
+
 
 function draw() {
-    if (keyIsDown(UP_ARROW)){
-        yRobot -= move;
-    } else if (keyIsDown(DOWN_ARROW)){
-        yRobot += move;
-    } else if (keyIsDown(LEFT_ARROW)){
+
+    // left edge
+    if (xRobot < 0){
+        xRobot += move;
+    //right edge
+    } else if (xRobot + robotWidth > width){
         xRobot -= move;
-    } else if (keyIsDown(RIGHT_ARROW)){
+    }
+    //top edge
+    if (yRobot < 0){
+        yRobot += move;
+
+    //bottom edge
+    } else if (yRobot + robotHeight > height){
+        yRobot -= move;
+    }
+
+
+//    if (colliding === true) {
+//
+//    }
+
+    if (keyIsDown(UP_ARROW)) {
+        yRobot -= move;
+    } else if (keyIsDown(DOWN_ARROW)) {
+        yRobot += move;
+    } else if (keyIsDown(LEFT_ARROW)) {
+        xRobot -= move;
+    } else if (keyIsDown(RIGHT_ARROW)) {
         xRobot += move;
     }
+
+//
+//    if (keyIsDown(LEFT_ARROW)){
+//        xRobot -= move;
+//        if (colliding) {
+//            xBlikje -= blikjeMove;
+//            xRobot = xBlikje + blikjeWidth;
+//
+//        }
+//    } else if (keyIsDown(RIGHT_ARROW)){
+//        xRobot += move;
+//        if (colliding) {
+//            xBlikje += blikjeMove;
+//            xRobot = xBlikje - robotWidth;
+//
+//        }
+//    }
+//
+//
+//    if (keyIsDown(UP_ARROW)){
+//        yRobot -= move;
+//        if (colliding) {
+//            yBlikje -= blikjeMove;
+//            yRobot = yBlikje + blikjeHeight;
+//        }
+//    } else if (keyIsDown(DOWN_ARROW)){
+//        yRobot += move;
+//        if (colliding) {
+//            yBlikje += blikjeMove;
+//            yRobot = yBlikje - robotHeight;
+//        }
+//    }
+
+
+//    if (keyIsDown(UP_ARROW)){
+//        yRobot -= move;
+//        if (colliding) {
+//            yBlikje -= blikjeMove;
+////            yRobot -= 1;
+//        }
+//    } else if (keyIsDown(DOWN_ARROW)){
+//        yRobot += move;
+//        if (colliding) {
+//            yBlikje += blikjeMove;
+////            yRobot -= 1;
+//        }
+//    } else if (keyIsDown(LEFT_ARROW)){
+//        xRobot -= move;
+//        if (colliding) {
+//            xRobot += move;
+//////            if (xRobot + robotWidth > xBlikje ) {
+//////                push_right = true;
+//////            } else
+////            if (xBlikje + blikjeWidth >= xRobot) {
+////                xBlikje -= blikjeMove;
+//////              xRobot += 1;
+//////                push_right = false;
+//            //}
+//
+//        }
+//    } else if (keyIsDown(RIGHT_ARROW)){
+//        xRobot += move;
+//        if (colliding) {
+//            xRobot -= move;
+////            if (xRobot + robotWidth > xBlikje) {
+////                xBlikje += blikjeMove;
+//////              xRobot -= 1;
+//////                push_right = false;
+////
+////            }
+//
+//        }
+//    }
+//
+//    if (keyIsDown("Space")) {
+//        if (colliding === true) {
+//            can_collected = true;
+//        }
+//
+//    }
+//    } else if (keyIsDown(Space)) {
+//
+//    }
+
+    colliding = false;
+
+
+
+    if (xRobot < xBlikje + blikjeWidth &&
+        xRobot + robotWidth> xBlikje &&
+        yRobot < yBlikje + blikjeHeight
+        && yRobot + robotHeight > yBlikje) {
+
+        // colliding = true;
+        can_collected = true;
+
+    }
+
     background(244, 208, 111);
 
     fill(96, 108, 56);
     rect(539,339,150,150);
 
-    strokeWeight(5);
-
-    fill(195, 66, 63);
-    ellipse(xRobot, yRobot, 2*radius, 2*radius);
-
-    fill(0, 0, 0);
-    ellipse(xRobot, yRobot, 27, 27);
-
-    image(img, 100, 100, 54, 62);
 
 
 
-    // oude code -
-    // het werkte niet want de snelheid was gewoon nul geworden en als je pijlen gebruikt om te bewegen,
-    //was het gewoon x/yRobot +/- 0 - dus het veranderde gewoon niet meer
-
-
-
-    // xRobot en yRobot zijn eigenlijk de cordinaten van het midden van de cirkel
-    // radius is de helft van de cirkel breedte/hoogte
-    // en het zwarte randje rondom de cirkel is vijf pixels
-    //als de robot het rand aanraakt verplaatsen we die met 2 pixels terug
-
-    // left edge
-    if (xRobot < 0 + radius + 5){
-        xRobot += move;
-    //right edge
-    } else if (xRobot > width - radius - 5){
-        xRobot -= move;
-    }
-    //top edge
-    if (yRobot < 0 + radius + 5){
-        yRobot += move;
-
-    //bottom edge
-    } else if (yRobot > height - radius - 5){
-        yRobot -= move;
+    if (can_collected === false) {
+        image(img, xBlikje, yBlikje, blikjeWidth, blikjeHeight);
     }
 
-//    if xRobot
-//    if (!(x2 > x1 + lengte || x1 > x2 + lengte || y2 > y1 + breedte || y1 > y2 + breedte)){
-//    //Botsing!
-//    }
 
+    image(robot_img, xRobot, yRobot, robotWidth, robotHeight);
 
 };
 
